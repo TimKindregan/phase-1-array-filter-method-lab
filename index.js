297dab4bdebd5a3ed26fe3714296b7784134c73d
Tim Kindregan <timothyj.kindregan@gmail.com>
2022-12-30 15:15:02 -0500

    commit tree b2f0ac8663f751cdea028f3b895d3beea445d7fb

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+function findMatching (array, matchString) {
+    let matchingNames = array.filter(function (name) {
+        return name.toLowerCase() === matchString.toLowerCase();
+    })
+    return matchingNames;
+}
+
+function fuzzyMatch (array, chars) {
+    let matchChar = array.filter(function (name) {
+        return name.indexOf(chars) === 0;
+    })
+    return matchChar;
+}
+
+function matchName (array, nameString) {
+    let matchingName = array.filter(function(driver) {
+        return driver.name === nameString
+    })
+    return matchingName;
+}
